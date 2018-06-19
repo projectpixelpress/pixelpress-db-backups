b18005f99ca860cd009fb55eebb0b4a50226aee2
Evan <evan@projectpixelpress.com>
2018-06-19 12:39:09 -0500

    sends the real file to S3

--- index.js
+++ index.js
@@ -11,9 +11,10 @@
 	const dbio = require('mongodb-io-native');
 	const S3 = require('aws-sdk/clients/s3');
 	const s3 = new S3(getAWSCreds());
+	const fs = require('fs');
 
 	const backup = async host => {
-		const exportFileName = 'newfile';
+		const exportFileName = `backup-${host}-TODO-date`;
 		const config = {
 			host: host,
 			port: 27017,
@@ -25,29 +26,36 @@
 		});
 	};
 
-	const upload = (filename, bucket) => {
+	const upload = (filePath, bucket) => {
 		return new Promise(function(resolve, reject) {
-			const params = {
-				Bucket: bucket,
-				Key: filename,
-				Body: 'TODO read a file'
-			};
-			console.log('Uploading with', params);
-			s3.upload(params, (err, data) => {
+			fs.readFile(filePath, (err, data) => {
 				if (err) {
 					return reject(err);
 				}
-				return resolve(data);
+
+				const params = {
+					Bucket: bucket,
+					Key: filePath.replace('/tmp/', ''),
+					Body: data
+				};
+
+				console.log('Uploading with', params);
+				s3.upload(params, (err, data) => {
+					if (err) {
+						return reject(err);
+					}
+					return resolve(data);
+				});
 			});
+
 		});
 	};
 
-	// const filename = await backup('localhost');
-	const filename = 'test';
+	const filename = await backup('localhost');
 	return await upload(filename, 'xxxxxxx');
 })().then(
 	results => {
-		console.log(`Backup finished with ${results}`);
+		console.log(`Backup finished with ${JSON.stringify(results)}`);
 		process.exit();
 	},
 	error => {
